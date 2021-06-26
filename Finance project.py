10fb84169cfaeabcc500cafc87e28df5d621d120
bmargerison <bmargerison@outlook.com>
2021-06-26 09:44:24 +0100

    changed cosmetics of first graph

--- Finance project.py
+++ Finance project.py
@@ -1,22 +1,25 @@
+import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn
 
-
+#import KPI dashboard
 dashboard = pd.read_excel('C:\\Users\\Ben\\Documents\\git\\Finance-KPIs\\MRR data.xlsx', index_col=0)
 
-fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(2,3, figsize=(20,50))
-
+#create figures and define common axes
+fig, axs = plt.subplots(2,2, figsize=(30,30))
 month_labels = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar']
 
+#extract MRR data from dashbaord for MRR graph (axs[0,0])
 MRR_data = dashboard.iloc[[4,5,6,7],:].set_axis(month_labels, axis='columns')
 MRR_data_plot = MRR_data.swapaxes('index','columns')
-ax1.set_yticks([50000, 100000, 150000, 200000, 250000])
-ax1.set_yticklabels(['50k', '100k', '150k', '200k', '250k'])
-MRR_data_plot.plot.bar(ax=ax1)
 
+#plot axs[0,0]
+axs[0,0].set_yticks([50000, 100000, 150000, 200000, 250000])
+axs[0,0].set_yticklabels(['50k', '100k', '150k', '200k', '250k'])
+axs[0,0].set_title('Monthly Revenue CY vs. PYs')
+MRR_data_plot.plot.bar(ax=axs[0,0], width=0.9, linewidth=20)
 
 
 print(MRR_data_plot)
-
-
 plt.show()
